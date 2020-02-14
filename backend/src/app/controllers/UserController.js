e24c56280d7fb241a1b858aa30eb781c111fd60e
Wil-g2 <wil-g2@hotmail.com>
2020-02-14 01:48:32 -0300

    feat: added User controller

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,24 @@
+import User from '../models/User';
+import i18n from 'i18n';
+
+class UserController {
+  async store(req, res){
+    try{
+      const { email } = req.body;
+      console.log(email);
+      const userExist = await User.findOne({ where: { email }});
+
+      if (userExist) {
+        return res.status(400).json({ error: i18n.__('ExistUserError')});
+      }
+
+      const user = await User.create(req.body);
+      return res.json(user);
+    }catch (error) {
+      console.log(error);
+    }
+
+  }
+}
+
+export default new UserController();
