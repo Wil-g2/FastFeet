4fe658a25d578825020df3b85dd7ad4973d1bbc3
Wil-g2 <wil-g2@hotmail.com>
2020-02-15 01:49:54 -0300

    update: change route for test user create

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import i18n from 'i18n';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
+routes.post('/', UserController.store );
 routes.get('/', (req, res) => {
-  return res.json({ msg: i18n.__('message')});
-});
-
+  return res.json({msg: i18n.__('message')});
+})
 export default routes;
