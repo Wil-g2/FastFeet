9fd697e2763d65b4e73973fbb3dae53473e7e5ab
Wil-g2 <wil-g2@hotmail.com>
2020-02-17 16:28:29 -0300

    feat: added a session method

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret:'ee9c57b6a88975bd6f252dc284d1c3e3',
+  expiresIn: '7d',
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import i18n from 'i18n';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store );
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
 routes.get('/', (req, res) => {
   return res.json({msg: i18n.__('message')});
 })
