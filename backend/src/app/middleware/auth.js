cc0850ca8c90d0baef1928ecf33596a31055b13c
Wil-g2 <wil-g2@hotmail.com>
2020-02-19 17:21:41 -0300

    fix: fix error message

--- backend/src/app/middleware/auth.js
+++ backend/src/app/middleware/auth.js
@@ -8,7 +8,7 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({error: i18n.__('TokenNotProvided')})
+    return res.status(401).json({error: i18n.__('TokenNotProvided')});
   }
 
   const [, token ] = authHeader.split(' ');
@@ -19,6 +19,6 @@ export default async (req, res, next) => {
     req.userId = decoded.id;
     return next();
   } catch ( err) {
-    return res.status(401).json({ error: i18n__('InvalidToken')});
+    return res.status(401).json({ error: i18n.__('InvalidToken')});
   }
 }
