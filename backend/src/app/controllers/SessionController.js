6b31565bdaa37eae06a9c18b83955b788185b6fc
Wil-g2 <wil-g2@hotmail.com>
2020-02-22 18:08:24 -0300

    feat: added validation with yup in sessionController and UserController

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,10 +1,24 @@
 import jwt from 'jsonwebtoken';
 import i18n from '../../config/i18n';
+import * as Yup from 'yup';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: i18n.__('ValidationErrosYup')});
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -27,7 +41,7 @@ class SessionController {
       token: jwt.sign({ id }, authConfig.secret ,{
         expiresIn: authConfig.expiresIn
       })
-    })
+    });
   }
 }
 
--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,9 +1,24 @@
+import * as Yup from 'yup';
 import i18n from 'i18n';
 import User from '../models/User';
 
 class UserController {
   async store(req, res){
     try{
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string()
+          .email()
+          .required(),
+        password: Yup.string()
+          .required()
+          .min(6),
+      });
+
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: i18n.__('ValidationErrosYup')});
+      }
+
       const { email } = req.body;
 
       const userExist = await User.findOne({ where: { email }});
@@ -25,12 +40,30 @@ class UserController {
   }
 
   async update(req, res) {
+
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: i18n.__('ValidationErrosYup')});
+    }
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
 
     //verify new email is different and if email exist in database
-    if (email !== user.email) {
+    if (email && email !== user.email) {
       const userExist = await User.findOne({ where: { email }});
       if (userExist) {
         return res.status(400).json({ error: i18n.__('ExistUserError')});
